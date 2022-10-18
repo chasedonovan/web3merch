e3874f1225a7343848c86bd48ca3021dec23a3ae
Chase Donovan <chase.donovan@aol.com>
2022-10-18 12:52:29 -0700

    👍 IMPROVE: apis built but not configured

---
+++ src/pages/api/checkoutcomplete.tsx
@@ -0,0 +1,31 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import Error from "next/error";
+
+// type Data = {
+//   name: string;
+//   success: boolean;
+// };
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<unknown>
+) {
+  try {
+    const cart = req.body.cart;
+    const info = req.body.info;
+    const externalResponse = await fetch(`${process.env.API_URL}/checkout`, {
+      headers: {
+        Authorization: `Bearer ${process.env.API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({cart, info}),
+    });
+
+    res.status(200).send({cart, info});
+    // res.status(200).send(externalResponse);
+  } catch (e) {
+    res.status(500).send({ error: e });
+  }
+}
