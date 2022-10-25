3c59aecfe51189118f2ff65765995c4fb2523f23
Jonas Hagman <jonasfeldhagman@gmail.com>
2022-10-25 16:09:46 +0300

    added example cart return value

---
+++ src/data/cart.js
@@ -0,0 +1,7 @@
+export const cart = {uuid: "a1c1b947-d192-4e76-8f0d-11efde2e97df", 
+    stake_key: "stake1u8h2kqxjxene8lt567sylcw72r53eu9qpz03crrm5nzwgucuqrwy5", 
+    transaction_id: "", 
+    cartItems: [{variant_id: 5, price: 75000000, quantity: 2}, 
+        {variant_id: 6, price: 75, quantity: 1}]
+        , shipping_price: 50000000, price: 225000000
+    };
