6ed73b951f26b163db797903584f100671b7c0a6
Jonas Hagman <jonasfeldhagman@gmail.com>
2022-11-03 10:46:07 +0200

    next payment apis

--- src/pages/api/cart/pay.tsx
+++ src/pages/api/cart/pay.tsx
@@ -1,23 +1,57 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import Error from "next/error";
-import NowPaymentsApi from "@nowpaymentsio/nowpayments-api-js";
-
-const npApi = new NowPaymentsApi({ apiKey: `${process.env.NOW_PAYMENTS}` }); // your api key
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<unknown>
 ) {
   try {
-      const payment = await npApi.createPayment({
-        price_amount: req.body.cart.totalPrice,
-        price_currency: "USD",
-        pay_currency: "ADA",
-      });
-      
-      res.status(200).json(payment);
-    } catch (e) {
+    const cart = req.body.cart;
+
+    const cartResponse = await getCart(cart.uuid);
+
+    const externalResponse = await fetch(
+      `${process.env.NOWPAYMENT_API_URL}/v1/payment?currency_from=USD&currency_to=ADA`,
+      {
+        headers: {
+          "x-api-key": `${process.env.NOWPAYMENT_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(`
+          "price_amount": ${cartResponse.total_price},
+          "price_currency": "usd",
+          "pay_currency": "ada",
+          "ipn_callback_url": "https://nowpayments.io",
+          "order_id": ${cartResponse.uuid},
+          "order_description": "GoatTribe merch"`),
+      }
+    );
+    const externalResponseData = await externalResponse.json();
+    console.log("Pay", externalResponseData);
+    res.status(200).json(externalResponseData);
+  } catch (e) {
     res.status(500).send({ error: e });
+  }
+}
+
+async function getCart(cart_uuid: string) {
+  try {
+    const cartResp = await fetch(
+      `${process.env.API_URL}/merch/cart/${cart_uuid}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        method: "GET",
+      }
+    );
+    const cartData = await cartResp.json();
+    console.log("Get cart", cartData);
+    return cartData;
+  } catch (e) {
+    throw e;
   }
 }
--- src/pages/api/cart/update.tsx
+++ src/pages/api/cart/update.tsx
@@ -26,8 +26,56 @@ export default async function handler(
     );
     const externalResponseData = await externalResponse.json();
     console.log("update cart", externalResponseData);
+
+    const minPayment = await minimumPayment();
+
+    if (minPayment.min_amount > cart.subTotalPrice) {
+      const estPayment = await estimatedPayment(cart);
+      externalResponseData.estimatedTotal = estPayment.estimated_amount;
+    }
+
     res.status(200).json(externalResponseData);
   } catch (e) {
     res.status(500).send({ error: e });
+  }
+}
+
+async function minimumPayment() {
+  try {
+    const minimumPaymentResponse = await fetch(
+      `${process.env.NOWPAYMENT_API_URL}/v1/min-amount?currency_from=usd&currency_to=ada`,
+      {
+        headers: {
+          "x-api-key": `${process.env.NOWPAYMENT_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        method: "GET",
+      }
+    );
+    const minimumPaymentData = await minimumPaymentResponse.json();
+    console.log("Minimum payment", minimumPaymentData);
+    return minimumPaymentData;
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function estimatedPayment(externalResponseData: any) {
+  try {
+    const estPaymentResponse = await fetch(
+      `${process.env.NOWPAYMENT_API_URL}/v1/min-amount?estimate?amount=${externalResponseData.totalPrice}&currency_from=usd&currency_to=ada`,
+      {
+        headers: {
+          "x-api-key": `${process.env.NOWPAYMENT_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        method: "GET",
+      }
+    );
+    const estPaymentData = await estPaymentResponse.json();
+    console.log("Estimated payment", estPaymentData);
+    return estPaymentData;
+  } catch (e) {
+    throw e;
   }
 }
