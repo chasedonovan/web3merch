56538c81797e42ce274e6695bf4783043b8494f4
Jonas Hagman <jonasfeldhagman@gmail.com>
2022-11-04 16:07:30 +0200

    some bug

--- src/components/merch/Navbar.tsx
+++ src/components/merch/Navbar.tsx
@@ -12,18 +12,18 @@ type Props = {
 
 export default function Navbar(props: Props) {
   const { isConnected } = useWalletContext();
-  const { addedModal, setAddedModal, maxModal, setMaxModal } = useMerchContext();
+  const { addedModal, setAddedModal, maxModal, setMaxModal } =
+    useMerchContext();
 
   useEffect(() => {
     if (addedModal) {
       setTimeout(() => {
         setAddedModal(false);
       }, 2500);
-    }
-      else if (maxModal) {
-        setTimeout(() => {
-          setMaxModal(false);
-        }, 2500);
+    } else if (maxModal) {
+      setTimeout(() => {
+        setMaxModal(false);
+      }, 2500);
     }
   }, [addedModal, maxModal]);
 
@@ -78,7 +78,9 @@ export default function Navbar(props: Props) {
             <div className="absolute top-28 right-0 w-full h-full bg-black bg-opacity-50 z-50 flex flex-col justify-center items-center">
               <div className="bg-white rounded-lg p-4">
                 <div className="flex flex-col justify-center items-center">
-                  <div className="text-2xl text-gray-800 font-bold">Added to Cart!</div>
+                  <div className="text-2xl text-gray-800 font-bold">
+                    Added to Cart!
+                  </div>
                   <div className="text-sm text-gray-500">
                     Check out your cart to complete your purchase.
                   </div>
@@ -98,9 +100,12 @@ export default function Navbar(props: Props) {
             <div className="absolute top-28 right-0 w-full h-full bg-black bg-opacity-50 z-50 flex flex-col justify-center items-center">
               <div className="bg-white rounded-lg p-4">
                 <div className="flex flex-col justify-center items-center">
-                  <div className=" text-gray-800 font-bold">Sorry, only one is allowed.</div>
+                  <div className=" text-gray-800 font-bold">
+                    Sorry, only one is allowed.
+                  </div>
                   <div className="text-sm text-gray-600">
-                    Keep up in our Discord or Twitter if you don't want to miss the restocking.
+                    Keep up in our Discord or Twitter if you don&apos;t want to
+                    miss the restocking.
                   </div>
                   <button
                     className="bg-black text-white rounded-lg px-4 py-2 mt-4"
